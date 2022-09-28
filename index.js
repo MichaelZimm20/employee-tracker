6168da73d1113f24a68d8cb6326709edc6853699
DJMIKE20 <m.zimm20@gmail.com>
2022-09-28 15:23:16 -0400

    added switch statement to handle what user selects from list, and modularize viewAll and addUpdate functions

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 // packages and connections 
 const db = require('./db/connection');
+const inquirer = require('inquirer');
+require('console.table');
+
+// Initial prompt for app
+function initialPrompt() {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'employment',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Departments', 
+                'View All Roles', 
+                'View All Employees', 
+                'Add a Department', 
+                'Add a Role',
+                'Add a Employee',
+                'Update an Employee Role'
+            ]
+        })
+        .then((answers) => {
+            // Switch statement to handle if user selects from initialPrompt choices
+            switch (answers.employment) {
+                case 'View All Departments':
+                    viewAllDepartments();
+                    break;
+            
+                default:
+                    break;
+            }
+        })
+}
+
+initialPrompt();
